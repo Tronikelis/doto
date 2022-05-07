eafef7ff7fbd25fea94838998c917bd39bc579b3
Tronikel <contactdonatas@gmail.com>
2022-05-07 14:01:50 +0300

    comment aggregation logic finishing up (without score rating)

--- server/src/routes/test/index.ts
+++ server/src/routes/test/index.ts
@@ -2,11 +2,15 @@ import { Resource } from "fastify-autoroutes";
 
 import { commentModel } from "@mongo";
 
+import { Datum, DeepReplies, MongoAggregationComments } from "./.types";
+
 const handler: any = async () => {
     const userId = "626e7569b48beadf4e36f22b";
+    const count = 25;
+    const page = 1;
 
     const comments = await commentModel
-        .aggregate([
+        .aggregate<MongoAggregationComments>([
             // get the correct comment/thread
             { $match: { description: "1212" } },
 
@@ -19,7 +23,7 @@ const handler: any = async () => {
                     connectToField: "_id",
                     as: "deepReplies",
                     depthField: "depth",
-                    maxDepth: 8,
+                    maxDepth: 10,
                 },
             },
 
@@ -63,7 +67,7 @@ const handler: any = async () => {
                                                 default: null,
                                             },
                                         },
-                                        replies: null,
+                                        replies: { $gt: [{ $size: "$$reply.replies" }, 0] },
                                     },
                                 ],
                             },
@@ -78,16 +82,73 @@ const handler: any = async () => {
                     deepReplies: 1,
                 },
             },
+            {
+                $unwind: "$deepReplies",
+            },
+            {
+                $sort: {
+                    "deepReplies.date": -1,
+                },
+            },
+            {
+                $group: {
+                    _id: "$deepReplies.depth",
+                    count: { $sum: 1 },
+                    data: { $push: "$$ROOT" },
+                },
+            },
+            {
+                $sort: {
+                    _id: 1,
+                },
+            },
+            {
+                $addFields: {
+                    data: {
+                        $slice: ["$data", (page - 1) * count, count],
+                    },
+                },
+            },
         ])
         .allowDiskUse(true);
 
-    await commentModel.populate(comments[0].deepReplies, {
+    await commentModel.populate(comments, {
         model: "User",
-        path: "author",
+        path: "data.deepReplies.author",
         select: ["nickname", "avatar"],
     });
 
-    return comments;
+    const root = { ...comments[0].data[0].deepReplies };
+    const reducedComments = comments.reduce(
+        (prev: any, cur) => [...prev, ...cur.data],
+        []
+    ) as Datum[];
+
+    // generate a tree structure -> reducedComments <=> replyTo
+    // basically recursively traverses every comments reply
+    // and populates the replies with the reducedComments that replyTo === own _id
+    const generate = (reply: DeepReplies) => {
+        const id = reply._id.toString();
+        const replies = reducedComments
+            .filter(({ deepReplies }) => deepReplies.replyTo?.toString() === id)
+            .reduce((prev: any, curr) => [...prev, curr.deepReplies], []) as DeepReplies[];
+
+        if (replies.length < 1) return;
+
+        reply.replies = replies as any;
+        replies.forEach(reply => generate(reply));
+    };
+    generate(root);
+
+    // calculate next based on root replies (depth 1)
+    const total = comments.find(({ _id }) => _id === 1)?.count || 0;
+
+    return {
+        count,
+        page,
+        next: count < total,
+        data: root.replies,
+    };
 };
 
 export default (): Resource => ({
